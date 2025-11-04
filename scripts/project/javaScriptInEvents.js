b147cb2147788ebab443a728733f80cf893fd8cc
thinh1995 <cuongthinhtuan2006@gmail.com>
2025-11-04 14:07:43 +0700

    master: init source

---
+++ scripts/project/javaScriptInEvents.js
@@ -0,0 +1,11 @@
+
+
+const scriptsInEvents = {
+
+	async EventSheetHome_Event50_Act5(runtime, localVars)
+	{
+		window.top.location.reload();
+	}
+};
+
+globalThis.C3.JavaScriptInEvents = scriptsInEvents;
